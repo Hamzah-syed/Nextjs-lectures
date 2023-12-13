9cd9e721f781990d40680e137525bf84c3113bd6
hamzah-syed <hamzah.syed17@gmail.com>
2023-12-14 00:15:04 +0500

    class 6 added

---
+++ class6/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+export default function Home() {
+  return (
+    <>
+    
+    </>
+
+  )
+}
